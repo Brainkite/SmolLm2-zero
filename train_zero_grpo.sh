97a315b2473c29ae06ca85b184516e463805cea7
antoninsumner <antonin.sumner@gmail.com>
2025-02-05 14:12:36 +0100

    Dynamically configure VLLM device based on available GPUs
    - Update train_zero_grpo.sh to count and export number of GPUs
    - Modify YAML configs to use dynamic VLLM device selection
    - Use environment variable `VLLM_CUDA_IDX` to automatically select the last GPU for VLLM

--- train_zero_grpo.sh
+++ train_zero_grpo.sh
@@ -1,6 +1,11 @@
 # First, make sure you're logged into wandb
 python -c "import torch; print('CUDA',torch.cuda.is_available())"
 
+# Count the number of GPUs and set N_GPUS
+N_GPUS=$(nvidia-smi -L | wc -l)
+export N_PROC=$((N_GPUS - 1))
+export VLLM_CUDA_IDX=$((N_GPUS - 1))
+
 accelerate launch \
   --num_processes $N_PROC \
   --config_file training/configs/accelerate_configs/deepspeed_zero3.yaml \
